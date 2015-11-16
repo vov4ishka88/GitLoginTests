ad1f2d915e24f4eb6cb9918676b0d55918d3aa18
Vlad Petrov <Vlad Petrov>
2015-11-16 20:45:07 +0200

    Initial commit

---
+++ UnitTestProject5/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace UnitTestProject5
+{
+    static class Helpers
+    {
+        public static bool IsElementPresent(IWebDriver driver, By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UnitTestProject5/LoginTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace UnitTestProject5
+{
+    [TestClass]
+    public class FacebookLoginTests
+    {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Url = "https://github.com/session";
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void InvalidCredentialsLeadToErrorPage()
+        {
+            // init
+
+            // act
+            var emailTextBox = driver.FindElement(By.Id("login_field"));
+            var passwordTextBox = driver.FindElement(By.Id("password"));
+
+            emailTextBox.SendKeys("notValid@gmail.com");
+            passwordTextBox.SendKeys("lalala");
+
+            passwordTextBox.SendKeys(Keys.Return);
+
+            // assert
+            Assert.IsTrue(Helpers.IsElementPresent(driver, By.ClassName("flash-error")));
+        }
+
+        [TestMethod]
+        public void ClickingSignUpLeadsToSignUpPage()
+        {
+            // init
+
+            // act
+            IWebElement signUp = driver.FindElement(By.CssSelector("a[href=\"/join\"]"));
+            signUp.Click();
+
+            // assert
+            Assert.IsTrue(Helpers.IsElementPresent(driver, By.ClassName("signup")));
+        }
+    }
+}
